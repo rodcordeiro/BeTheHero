15e1d6193529dd0f942c3f38c920b800233a8fdf
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-04-05 00:53:15 -0300

    Update mobile/

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.15.12:3333'
+    baseURL: 'https://cordeiro-bethehero-backend.herokuapp.com'
 });
 
 export default api;
